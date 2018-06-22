becf3ce4d8848083c85b18f771e8e2605475224c
Vickie Liu <vicky.liu.61@gmail.com>
2018-06-22 15:38:43 -0400

    Create test.java
    Initial creation

---
+++ test.java
@@ -0,0 +1,19 @@
+package test.java.hello;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by vickie.liu on 06/22/2018.
+ */
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import main.java.hello.Greeter;
+import org.junit.Test;
+
+public class GreeterTest {
+
+    public GreeterTest() {};
+
+}
